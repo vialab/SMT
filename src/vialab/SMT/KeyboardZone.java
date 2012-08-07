21d53fe6c7b714c89efaa031949447eb0d87a4be
Zachary Cook <zachary.cook@mycampus.uoit.ca>
2012-08-07 17:29:38 -0400

    keyboardZone updated
    adds some modifier keys, and makes sure they act properly

--- src/vialab/SMT/KeyboardZone.java
+++ src/vialab/SMT/KeyboardZone.java
@@ -20,68 +20,189 @@ public class KeyboardZone extends Zone {
 		private static final long serialVersionUID = -3237916182106172342L;
 	};
 
+	private static String toString(Keys key) {
+		if (!key.isModifier) {
+			if (key.keyCode == KeyEvent.VK_SPACE) {
+				return "Space";
+			}
+			return Character.toString(Character.toUpperCase(key.keyChar));
+		}
+		else {
+			switch (key.keyCode) {
+			case KeyEvent.VK_SHIFT:
+				return "Shift";
+			case KeyEvent.VK_CONTROL:
+				return "Ctrl";
+			case KeyEvent.VK_ALT:
+				return "Alt";
+			}
+		}
+		return null;
+	}
+
 	private class KeyZone extends ButtonZone {
 		private Keys key;
 
 		public KeyZone(int x, int y, int width, int height, Keys key) {
-			super(x, y, width, height, Character.toString(key.keyChar).toUpperCase());
+			super(x, y, width, height, KeyboardZone.toString(key));
 			this.key = key;
+		}
+
+		@Override
+		public void draw() {
+			super.draw();
+			// make sure modifiers have the correct setting as they act
+			// differently than normal keys and should be unset even without a
+			// touchUp event, although really just a hack, as touchUp should be
+			// generated whenever a touch is unassigned from a zone
+			if (isButtonDown()) {
+				modifierDown();
+			}
+			else {
+				modifierUp();
+			}
+
 		}
 
 		@Override
 		public void touchDown(Touch touch) {
 			if (!isButtonDown()) {
-				// send key press using KeyEvent to listeners
-				for (KeyListener l : keyListeners) {
-					l.keyPressed(new KeyEvent(keyboardComponent, KeyEvent.KEY_PRESSED, System
-							.currentTimeMillis(), MODIFIERS, key.keyCode, key.keyChar));
-				}
+				keyDown();
+			}
+			super.touchDown(touch);
+		}
+
+		private void keyDown() {
+			char k = key.keyChar;
+			// if not undefined char and shift is on, set to upper case
+			if (key.keyChar != KeyEvent.CHAR_UNDEFINED && (MODIFIERS >> 6) % 2 == 1) {
+				k = Character.toUpperCase(key.keyChar);
+			}
+			// send key press using KeyEvent to listeners
+			for (KeyListener l : keyListeners) {
+				l.keyPressed(new KeyEvent(keyboardComponent, KeyEvent.KEY_PRESSED, System
+						.currentTimeMillis(), MODIFIERS, key.keyCode, k));
 			}
 
-			super.touchDown(touch);
+			modifierDown();
+		}
+
+		private void modifierDown() {
+			switch (key.keyCode) {
+			case KeyEvent.VK_SHIFT:
+				if ((MODIFIERS >> 6) % 2 == 0) {
+					// add modifier only if not down already
+					MODIFIERS += KeyEvent.SHIFT_DOWN_MASK;
+
+				}
+				break;
+			case KeyEvent.VK_CONTROL:
+				if ((MODIFIERS >> 7) % 2 == 0) {
+					// add modifier only if not down already
+					MODIFIERS += KeyEvent.CTRL_DOWN_MASK;
+
+				}
+				break;
+			case KeyEvent.VK_ALT:
+				if ((MODIFIERS >> 8) % 2 == 0) {
+					// add modifier only if not down already
+					MODIFIERS += KeyEvent.ALT_DOWN_MASK;
+
+				}
+				break;
+			default:
+				break;
+			}
 		}
 
 		@Override
 		public void touchUp(Touch touch) {
 			if (isButtonDown()) {
-				// send key release and typed using KeyEvent to listeners
-				for (KeyListener l : keyListeners) {
-					l.keyReleased(new KeyEvent(keyboardComponent, KeyEvent.KEY_RELEASED, System
-							.currentTimeMillis(), MODIFIERS, key.keyCode, key.keyChar));
-					if (true) {
-						l.keyTyped(new KeyEvent(keyboardComponent, KeyEvent.KEY_TYPED, System
-								.currentTimeMillis(), MODIFIERS, KeyEvent.VK_UNDEFINED, key.keyChar));
-					}
+				keyUp();
+			}
+			super.touchUp(touch);
+		}
+
+		private void keyUp() {
+			char k = key.keyChar;
+			// if not undefined char and shift is on, set to upper case
+			if (key.keyChar != KeyEvent.CHAR_UNDEFINED && (MODIFIERS >> 6) % 2 == 1) {
+				k = Character.toUpperCase(key.keyChar);
+			}
+			// send key release and typed using KeyEvent to listeners
+			for (KeyListener l : keyListeners) {
+				l.keyReleased(new KeyEvent(keyboardComponent, KeyEvent.KEY_RELEASED, System
+						.currentTimeMillis(), MODIFIERS, key.keyCode, k));
+				if (!key.isModifier) {
+					l.keyTyped(new KeyEvent(keyboardComponent, KeyEvent.KEY_TYPED, System
+							.currentTimeMillis(), MODIFIERS, KeyEvent.VK_UNDEFINED, k));
 				}
 			}
 
-			super.touchUp(touch);
+			modifierUp();
+		}
+
+		private void modifierUp() {
+			switch (key.keyCode) {
+			case KeyEvent.VK_SHIFT:
+				if ((MODIFIERS >> 6) % 2 == 1) {
+					// remove modifier only if down already
+					MODIFIERS -= KeyEvent.SHIFT_DOWN_MASK;
+
+				}
+				break;
+			case KeyEvent.VK_CONTROL:
+				if ((MODIFIERS >> 7) % 2 == 1) {
+					// remove modifier only if down already
+					MODIFIERS -= KeyEvent.CTRL_DOWN_MASK;
+
+				}
+				break;
+			case KeyEvent.VK_ALT:
+				if ((MODIFIERS >> 8) % 2 == 1) {
+					// remove modifier only if down already
+					MODIFIERS -= KeyEvent.ALT_DOWN_MASK;
+
+				}
+				break;
+			default:
+				break;
+			}
 		}
 	}
 
-	private static final int DEFAULT_HEIGHT = 200;
+	private static final int DEFAULT_HEIGHT = 250;
 	private static final int DEFAULT_WIDTH = 500;
 
 	private enum Keys {
-		KY_1('1', KeyEvent.VK_0), KY_2('2', KeyEvent.VK_0), KY_3('3', KeyEvent.VK_0), KY_4('4',
-				KeyEvent.VK_0), KY_5('5', KeyEvent.VK_0), KY_6('6', KeyEvent.VK_0), KY_7('7',
-				KeyEvent.VK_0), KY_8('8', KeyEvent.VK_0), KY_9('9', KeyEvent.VK_0), KY_0('0',
-				KeyEvent.VK_0), KY_Q('Q', KeyEvent.VK_0), KY_W('W', KeyEvent.VK_0), KY_E('E',
-				KeyEvent.VK_0), KY_R('R', KeyEvent.VK_0), KY_T('T', KeyEvent.VK_0), KY_Y('Y',
-				KeyEvent.VK_0), KY_U('U', KeyEvent.VK_0), KY_I('I', KeyEvent.VK_0), KY_O('O',
-				KeyEvent.VK_0), KY_P('P', KeyEvent.VK_0), KY_A('A', KeyEvent.VK_0), KY_S('S',
-				KeyEvent.VK_0), KY_D('D', KeyEvent.VK_0), KY_F('F', KeyEvent.VK_0), KY_G('G',
-				KeyEvent.VK_0), KY_H('H', KeyEvent.VK_0), KY_J('J', KeyEvent.VK_0), KY_K('K',
-				KeyEvent.VK_0), KY_L('L', KeyEvent.VK_0), KY_Z('Z', KeyEvent.VK_0), KY_X('X',
-				KeyEvent.VK_0), KY_C('C', KeyEvent.VK_0), KY_V('V', KeyEvent.VK_0), KY_B('B',
-				KeyEvent.VK_0), KY_N('N', KeyEvent.VK_0), KY_M('M', KeyEvent.VK_0);
+		KEY_1('1', KeyEvent.VK_1, false), KEY_2('2', KeyEvent.VK_2, false), KEY_3('3',
+				KeyEvent.VK_3, false), KEY_4('4', KeyEvent.VK_4, false), KEY_5('5', KeyEvent.VK_5,
+				false), KEY_6('6', KeyEvent.VK_6, false), KEY_7('7', KeyEvent.VK_7, false), KEY_8(
+				'8', KeyEvent.VK_8, false), KEY_9('9', KeyEvent.VK_9, false), KEY_0('0',
+				KeyEvent.VK_0, false), KEY_Q('q', KeyEvent.VK_Q, false), KEY_W('w', KeyEvent.VK_W,
+				false), KEY_E('e', KeyEvent.VK_E, false), KEY_R('r', KeyEvent.VK_R, false), KEY_T(
+				't', KeyEvent.VK_T, false), KEY_Y('y', KeyEvent.VK_Y, false), KEY_U('u',
+				KeyEvent.VK_U, false), KEY_I('i', KeyEvent.VK_I, false), KEY_O('o', KeyEvent.VK_O,
+				false), KEY_P('p', KeyEvent.VK_P, false), KEY_A('a', KeyEvent.VK_A, false), KEY_S(
+				's', KeyEvent.VK_S, false), KEY_D('d', KeyEvent.VK_D, false), KEY_F('f',
+				KeyEvent.VK_F, false), KEY_G('g', KeyEvent.VK_G, false), KEY_H('h', KeyEvent.VK_H,
+				false), KEY_J('j', KeyEvent.VK_J, false), KEY_K('k', KeyEvent.VK_K, false), KEY_L(
+				'l', KeyEvent.VK_L, false), KEY_Z('z', KeyEvent.VK_Z, false), KEY_X('x',
+				KeyEvent.VK_X, false), KEY_C('c', KeyEvent.VK_C, false), KEY_V('v', KeyEvent.VK_V,
+				false), KEY_B('b', KeyEvent.VK_B, false), KEY_N('n', KeyEvent.VK_N, false), KEY_M(
+				'm', KeyEvent.VK_M, false), KEY_SHIFT(KeyEvent.CHAR_UNDEFINED, KeyEvent.VK_SHIFT,
+				true), KEY_CTRL(KeyEvent.CHAR_UNDEFINED, KeyEvent.VK_CONTROL, true), KEY_ALT(
+				KeyEvent.CHAR_UNDEFINED, KeyEvent.VK_ALT, true), KEY_SPACE(' ', KeyEvent.VK_SPACE,
+				false);
 
+		private final boolean isModifier;
 		private final char keyChar;
 		private final int keyCode;
 
-		Keys(char s, int code) {
+		Keys(char s, int code, boolean isModifier) {
 			keyChar = s;
 			keyCode = code;
+			this.isModifier = isModifier;
 		}
 
 		public String toString() {
@@ -122,12 +243,15 @@ public class KeyboardZone extends Zone {
 		for (int i = 0; i < 7; i++) {
 			this.add(new KeyZone(i * 50, 150, 50, 50, Keys.values()[29 + i]));
 		}
-		
-		for(Zone zone : this.children){
+		for (int i = 0; i < 4; i++) {
+			this.add(new KeyZone(i * 50, 200, 50, 50, Keys.values()[36 + i]));
+		}
+
+		for (Zone zone : this.children) {
 			zone.setDirect(true);
 		}
-		
-		//add the processing applet as a KeyListener by default
+
+		// add the processing applet as a KeyListener by default
 		this.addKeyListener(applet);
 	}
 
