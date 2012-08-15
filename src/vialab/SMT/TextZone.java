7b987078fa796fe872ad0e38e533442187244217
Zachary Cook <zachary.cook@mycampus.uoit.ca>
2012-08-14 21:46:21 -0400

    selectable WordZones in TextZone

--- src/vialab/SMT/TextZone.java
+++ src/vialab/SMT/TextZone.java
@@ -2,9 +2,51 @@ package vialab.SMT;
 
 import java.awt.event.KeyEvent;
 
+import processing.core.PFont;
+
 public class TextZone extends Zone {
 
+	private class WordZone extends Zone {
+
+		public String word;
+		private boolean selected = false;
+
+		public WordZone(int x, int y, int width, int height) {
+			super(x, y, width, height);
+			this.word = "";
+			TextZone.this.add(this);
+			this.setDirect(true);
+		}
+
+		@Override
+		public void touchUp(Touch touch) {
+			if (this.getTouches().isEmpty()) {
+				selected = !selected;
+			}
+			super.touchUp(touch);
+		}
+
+		@Override
+		public void draw() {
+			super.beginDraw();
+			fill(255);
+			if(selected){
+				fill(255,0,0);
+			}
+			rect(0, 0, width, height);
+			fill(0);
+			text(this.word, 0, 5, width, height);
+			super.endDraw();
+			super.draw();
+		}
+
+	}
+
 	public String text = "";
+
+	private WordZone currentWordZone;
+
+	private PFont font = applet.createFont("Arial", 16);
 
 	public TextZone(int x, int y, int width, int height) {
 		this(null, x, y, width, height, false);
@@ -20,9 +62,11 @@ public class TextZone extends Zone {
 
 	public TextZone(String name, int x, int y, int width, int height, boolean keysRecievedFromApplet) {
 		super(name, x, y, width, height);
+		this.currentWordZone = new WordZone(0, 0, 1, 20);
 		if (keysRecievedFromApplet) {
 			applet.registerKeyEvent(this);
 		}
+
 	}
 
 	@Override
@@ -30,16 +74,29 @@ public class TextZone extends Zone {
 		super.beginDraw();
 		fill(255);
 		rect(0, 0, width, height);
-		fill(0);
-		text(text, 10, 10, width, height);
 		super.endDraw();
 		super.draw();
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		System.out.print(e.getKeyChar());
-		this.text += e.getKeyChar();
+		textFont(font);
+		if (e.getKeyChar() == ' ') {
+			this.currentWordZone = new WordZone(currentWordZone.x
+					+ (int) textWidth(currentWordZone.word + e.getKeyChar()), currentWordZone.y, 1, 20);
+		}
+		else if (e.getKeyChar() == '\t') {
+			this.currentWordZone = new WordZone(currentWordZone.x
+					+ (int) textWidth(currentWordZone.word + e.getKeyChar()), currentWordZone.y, 1, 20);
+		}
+		else if (e.getKeyChar() == '\n') {
+			this.currentWordZone = new WordZone(0, currentWordZone.y + 20, 1, 20);
+		}
+		else {
+			this.currentWordZone.word += e.getKeyChar();
+			this.currentWordZone.setData(currentWordZone.x, currentWordZone.y,
+					(int) textWidth(currentWordZone.word), currentWordZone.height);
+		}
 		super.keyTyped(e);
 	}
 
