119bb28cb9df7301b1b53f6e3e9e2c2917993edf
Zachary Cook <zachary.cook@mycampus.uoit.ca>
2012-09-13 12:22:58 -0400

    rename mouseToTUIO package to TUIOSource

--- src/vialab/TUIOSource/MouseToTUIO.java
+++ src/vialab/TUIOSource/MouseToTUIO.java
@@ -21,7 +21,7 @@
  * this library; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package vialab.mouseToTUIO;
+package vialab.TUIOSource;
 
 import java.awt.Point;
 import java.util.Enumeration;
