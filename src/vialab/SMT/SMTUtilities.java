54d33e379963a81e10295b2673b3a82b8549a450
Zachary Cook <zachary.cook@mycampus.uoit.ca>
2013-01-17 16:37:36 -0500

    fix checkImpl() so that it doesnt have errors
    We need to stop recursively checkImpl() when at Zone, as otherwise we
    check past it, and cause errors
    
    Also some formatting

--- src/vialab/SMT/SMTUtilities.java
+++ src/vialab/SMT/SMTUtilities.java
@@ -177,16 +177,19 @@ public final class SMTUtilities {
 	 * @return Whether the given class has a method with the given Prefix
 	 */
 	public static boolean checkImpl(String methodPrefix, Class<?>... parameters) {
-		if(parameters[0] == null){
+		if (parameters[0] == null) {
 			System.err.println("Error: CheckImpl() first class parameter was null.");
 			return false;
 		}
-		
-		if(!(Zone.class.isAssignableFrom(parameters[0]))){
-			System.err.println("Error: CheckImpl() first class parameter was not Zone or a subclass, please give the current Zone class (using this.getClass()) as the first class parameter.");
+
+		if (!(Zone.class.isAssignableFrom(parameters[0]))) {
+			System.err
+					.println("Error: CheckImpl() first class parameter ("
+							+ parameters[0]
+							+ ") was not Zone or a subclass, please give the current Zone class (using this.getClass()) as the first class parameter.");
 			return false;
 		}
-		
+
 		Method impl = null;
 		// only check for method impl if the parent class has the method too,
 		// because by default this method is only called when warnMissing is
@@ -196,7 +199,8 @@ public final class SMTUtilities {
 		try {
 			Class<?>[] firstRemoved = new Class<?>[parameters.length - 1];
 			System.arraycopy(parameters, 1, firstRemoved, 0, parameters.length - 1);
-			if (parameters[0].getSuperclass().getDeclaredMethod(methodPrefix + "Impl", firstRemoved) != null) {
+			if (parameters[0].getSuperclass()
+					.getDeclaredMethod(methodPrefix + "Impl", firstRemoved) != null) {
 				try {
 					// get the method if the class declared the prefix+Impl
 					// method,
@@ -231,12 +235,16 @@ public final class SMTUtilities {
 		}
 		catch (Exception e) {}
 
-		// check if a super-class implements it, and the superclass is not Zone
+		// check if a super-class implements it, and the superclass is not Zone,
+		// and also that this class is not Zone, as we shouldn't check the
+		// superclass in that case either
 		if (impl == null && parameters[0].getSuperclass() != null
-				&& parameters[0].getSuperclass() != Zone.class) {
+				&& parameters[0].getSuperclass() != Zone.class && parameters[0] != Zone.class) {
 			Class<?> superClass = parameters[0].getSuperclass();
-			parameters[0] = superClass;
-			if (checkImpl(methodPrefix, parameters)) {
+			Class<?>[] firstSupered = new Class<?>[parameters.length];
+			System.arraycopy(parameters, 0, firstSupered, 0, parameters.length);
+			firstSupered[0] = superClass;
+			if (checkImpl(methodPrefix, firstSupered)) {
 				return true;
 			}
 		}
