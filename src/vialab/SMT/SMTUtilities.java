vialab/SMT

commit 5a694c393ab22467754aa32b31ddc6a84f895f8d

fix KeyboardZone to work with ButtonZone changes
Also reduce unneeded picking and fix checkImpl for pressImpl()

src/vialab/SMT/SMTUtilities.java
@@ -232,16 +232,11 @@ public final class SMTUtilities {
 		}
 
 		Method impl = null;
-		// only check for method impl if the parent class has the method too,
-		// because by default this method is only called when warnMissing is
-		// true when getZoneMethod(...) is called, so we only give a warning
-		// when the extending class has no [methodPefix+impl]() method and the
-		// pressMethod cannot be found, which is exactly what we want to occur.
+		//TODO:Clean up and comment this, as it has caused many problems
 		try {
 			Class<?>[] firstRemoved = new Class<?>[parameters.length - 1];
 			System.arraycopy(parameters, 1, firstRemoved, 0, parameters.length - 1);
-			if (parameters[0].getSuperclass()
-					.getDeclaredMethod(methodPrefix + "Impl", firstRemoved) != null) {
+			if (Zone.class.getDeclaredMethod(methodPrefix + "Impl", firstRemoved) != null) {
 				try {
 					// get the method if the class declared the prefix+Impl
 					// method,

src/vialab/SMT/ButtonZone.java
@@ -33,8 +33,6 @@ public class ButtonZone extends Zone {
 
 	private float angle = 0;
 
-	protected boolean buttonDown = false;
-
 	protected int deactivatedColor = 255;
 
 	protected int deactivatedTextColor = 175;

src/vialab/SMT/KeyboardZone.java
@@ -57,7 +57,7 @@ public class KeyboardZone extends Zone {
 				drawImpl(color(255, alpha), color(175, alpha));
 			}
 			else {
-				if (buttonDown) {
+				if (isButtonDown()) {
 					drawImpl(color(keyPressedColor, alpha), color(textColor, alpha));
 				}
 				else {
@@ -107,8 +107,7 @@ public class KeyboardZone extends Zone {
 	
 		@Override
 		public void touchMovedImpl(Touch touch) {
-			this.setButtonDown();
-			if (!keyDown && this.isButtonDown()) {
+			if (!keyDown) {
 				keyDown();
 			}
 		}
@@ -133,7 +132,7 @@ public class KeyboardZone extends Zone {
 		}
 	
 		@Override
-		public void pressImpl() {
+		protected void pressImpl() {
 			//toggle Caps Lock
 			if(key.keyCode ==KeyEvent.VK_CAPS_LOCK){
 				capsLockOn = !capsLockOn;

src/vialab/SMT/SMTTouchManager.java
@@ -87,8 +87,9 @@ class SMTTouchManager {
 		for (Touch t : currentTouchState) {
 			if (previousTouchState.contains(t.sessionID)) {
 				SMTUtilities.invoke(touchMoved, applet);
-				Zone z = picker.pick(t);
+				Zone z = null;
 				if (!t.isAssigned()) {
+					z = picker.pick(t);
 					// Assign the touch to the picked Zone, as long as the touch
 					// is not grabbed
 					if (z != null) {
@@ -96,13 +97,25 @@ class SMTTouchManager {
 					}
 				}
 				else {
+					boolean first = true;
 					for (Zone zone : t.getAssignedZones()) {
-						// if the zone defines a press method, make sure to
-						// unassign when we no longer pick to the Zone, meaning
-						// that the touchUp can rely on the previous pick of the
-						// Touch to determine if the zone was pressed
-						if ((zone.press || zone.pressMethod != null) && z != zone) {
-							zone.unassign(t);
+						System.out.println(zone.press+" "+zone.pressMethod);
+						if (zone.press || zone.pressMethod != null){
+							// if the zone defines a press method, make sure to
+							// unassign when we no longer pick to the Zone, meaning
+							// that the touchUp can rely on the previous pick of the
+							// Touch to determine if the zone was pressed
+							if (first){
+								z = picker.pick(t);
+								first = false;
+							}
+							if (z != zone) {
+								zone.unassign(t);
+								// Assign the touch to the picked Zone
+								if (z != null) {
+									z.assign(t);
+								}
+							}
 						}
 					}
 				}

commit 896fc9a7d651543785201cb21d6793ea9dd9f877

fix checkImpl()
this was broken in 5a694c393ab22467754aa32b31ddc6a84f895f8d, trying to
get pressImpl() to work, but actually breaking it instead

src/vialab/SMT/SMTUtilities.java
@@ -236,7 +236,7 @@ public final class SMTUtilities {
 		try {
 			Class<?>[] firstRemoved = new Class<?>[parameters.length - 1];
 			System.arraycopy(parameters, 1, firstRemoved, 0, parameters.length - 1);
-			if (Zone.class.getDeclaredMethod(methodPrefix + "Impl", firstRemoved) != null) {
+			if (parameters[0].getSuperclass().getDeclaredMethod(methodPrefix + "Impl", firstRemoved) != null) {
 				try {
 					// get the method if the class declared the prefix+Impl
 					// method,