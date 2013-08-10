2a6a9b906d42462dc2c04d8ac62a870f944a60d6
Zachary Cook <zachary.cook@mycampus.uoit.ca>
2013-08-09 20:20:01 -0400

    add TextureZone, for drawing which changes rarely
    This Zone is optimized to look good and still be fast by only
    re-rendering content when needed, so it can use drawing methods that
    have a performance trade-off.
    
    To update this Zone call setModified(true), and its content will be
    updated during the next frame redraw

---
+++ src/vialab/SMT/TextureZone.java
@@ -0,0 +1,46 @@
+package vialab.SMT;
+
+/**
+ * TextureZone renders a Zone like a texture, requiring a call to setModified(true); to update it's contents
+ * It also uses defaults that look much better such as a higher smoothing level and vector accurate text,
+ * which would normally be too slow to draw every frame. Children of this Zone will also only render inside
+ * its bounds, and only when a call to setModified(true) is made on the parent TextureZone
+ */
+public class TextureZone extends Zone {
+
+	public TextureZone() {
+		this(null);
+	}
+
+	public TextureZone(String name) {
+		this(name, SMT.defaultRenderer);
+	}
+
+	public TextureZone(String name, String renderer) {
+		this(name, 0, 0, 1, 1, renderer);
+	}
+
+	public TextureZone(int x, int y, int width, int height) {
+		this(null, x, y, width, height, SMT.defaultRenderer);
+	}
+
+	public TextureZone(int x, int y, int width, int height, String renderer) {
+		this(null, x, y, width, height, renderer);
+	}
+
+	public TextureZone(String name, int x, int y, int width, int height) {
+		this(name, x, y, width, height, SMT.defaultRenderer);
+	}
+
+	public TextureZone(String name, int x, int y, int width, int height, String renderer) {
+		super(name, x, y, width, height, renderer);
+		this.setDirect(false);
+	}
+	
+	@Override
+	public void beginDraw(){
+		super.beginDraw();
+		smooth(8);
+		textMode(SHAPE);
+	}
+}
