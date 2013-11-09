d0dad7418485cd0293ff108596d37c42c3293bee
Erik Paluka <erik.paluka@uoit.ca>
2013-11-09 16:17:51 -0500

    Added setters and getters, comments, and more

--- src/vialab/SMT/TextZone.java
+++ src/vialab/SMT/TextZone.java
@@ -10,6 +10,10 @@ import processing.event.KeyEvent;
  */
 public class TextZone extends Zone {
 
+	/**
+	 * Zone used to render the text in the textZone
+	 *
+	 */
 	private class WordZone extends Zone {
 
 		public String word;
@@ -50,48 +54,307 @@ public class TextZone extends Zone {
 		public void drawImpl() {
 			textFont(font);
 			textSize(fontSize);
-			fill(255);
+			
 			if (blur) {
-				textFont(sFont);
-				if (selected) {
-					textFont(font);
+				if (!selected) {
+					textFont(sFont);
 				}
-				textSize(fontSize);
-				if (width > 0) {
+				
+				// Render a background for the text
+				if (width > 0 && txtBkg) {
 					noStroke();
 					rect(0, 0, width, height);
 				}
-				fill(0);
+
+
 				text(this.word, 0, 0, width, height);
 			}
 			else {
-				if (selected) {
-					fill(0, 0, 255, 127);
-				}
-				if (width > 0) {
+				// Render a background for the text				
+				if (width > 0 && txtBkg) {
+					if (selected) {
+						fill(selBkgRed, selBkgBlue, selBkgGreen, selBkgAlpha);
+					}
 					noStroke();
 					rect(0, 0, width, height);
 				}
-				fill(0);
+				
+				//Set the color of the text
+				if (selected) {
+					fill(selTxtRed, selTxtBlue, selTxtGreen, selTxtAlpha);
+				} else {
+					fill(txtRed, txtBlue, txtGreen, txtAlpha);
+				}
+				
 				text(this.word, 0, 0, width, height);
 			}
 		}
 
 	}
 
+	/** Sets if the text in the TextZone is selectable/highlightable */
 	private boolean selectable = false;
 
+	/** Zone used to render the text in the textZone */
 	private WordZone currentWordZone;
 
+	/** Font used for rendering the text */
 	private PFont font;
 
+	/** Font used for rendering the blurred text*/
 	private PFont sFont;
 
+	/** Bluring Flag */
 	private boolean blur = false;
 
+	/** Set the TextZone to receive input from the PApplet (keyboard)*/
 	private boolean keysFromApplet = false;
 
+	/** Font size used to render text*/
 	private float fontSize = 16;
+	
+	/** Text background color */
+	int bkgRed = 255, bkgGreen = 255, bkgBlue = 255, bkgAlpha = 255;
+	
+	/** Text color */
+	int txtRed = 0, txtGreen = 0, txtBlue = 0, txtAlpha = 255;
+	
+	/** Color of text background when it is selected */
+	int selBkgRed = 40, selBkgGreen = 80, selBkgBlue = 180, selBkgAlpha = 127;
+	
+	/** Color of the txt when it is selected */
+	int selTxtRed = 255, selTxtGreen = 255,  selTxtBlue= 255, selTxtAlpha = 255;
+	
+	/** Text background color flag */
+	boolean txtBkg = false;
+	
+	/**
+	 * Set the text background color
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @param a
+	 */
+	public void setBkgColor(int r, int g, int b, int a){
+		bkgRed = r;
+		bkgGreen = g;
+		bkgBlue = b;
+		bkgAlpha = a;
+	}
+	
+	/**
+	 * Set the text color
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @param a
+	 */
+	public void setTextColor(int r, int g, int b, int a){
+		txtRed = r;
+		txtGreen = g;
+		txtBlue = b;
+		txtAlpha = a;
+	}
+	
+	/**
+	 * Set the highlight color when the text in selected
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @param a
+	 */
+	public void setSelBkgColor(int r, int g, int b, int a){
+		selBkgRed = r;
+		selBkgGreen = g;
+		selBkgBlue = b;
+		selBkgAlpha = a;
+	}
+	
+	/**
+	 * Set the text color when the text is selected
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @param a
+	 */
+	public void setSelTextColor(int r, int g, int b, int a){
+		selTxtRed = r;
+		selTxtGreen = g;
+		selTxtBlue = b;
+		selTxtAlpha = a;
+	}
+	
+	/**
+	 * Gets the background color of the rectangle that the text is rendered over.
+	 * This rectangle is rendered if this.setDrawTextBkg(true) is used.
+	 * @return
+	 */
+	public int[] getBkgColor(){
+		return new int[]{bkgRed, bkgGreen, bkgBlue, bkgAlpha};
+	}
+	
+	/**
+	 * Gets the text color
+	 * @return
+	 */
+	public int[] getTextColor(){
+		return new int[]{txtRed, txtGreen, txtBlue, txtAlpha};
+	}
+	
+	/**
+	 * Gets the highlight color when the text is selected
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @param a
+	 * @return
+	 */
+	public int[] getSelBkgColor(int r, int g, int b, int a){
+			return new int[]{selBkgRed, selBkgGreen, selBkgBlue, selBkgAlpha};
+	}
+	
+	/**
+	 * Gets the color of the text when it is selected
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @param a
+	 * @return
+	 */
+	public int[] getSelTextColor(int r, int g, int b, int a){
+				return new int[]{selTxtRed, selTxtGreen, selTxtBlue, selTxtAlpha};
+	}
+	
+	/**
+	 * Sets the draw text background flag.
+	 * When true, the text will be rendered on a background rectangle 
+	 * @param flag
+	 */
+	public void setDrawTextBkg(boolean flag){
+		txtBkg = flag;
+	}
+	
+	/**
+	 * Get the draw text background flag.
+	 * When true, the text will be rendered on a background rectangle
+	 * @return
+	 */
+	public boolean getDrawTextBkg(){
+		return txtBkg;
+	}
+	
+	/**
+	 * Sets the word zone used to render the text in the textZone
+	 * @param wZone
+	 */
+	public void setWordZone(WordZone wZone){
+		currentWordZone = wZone;
+	}
+	
+	/**
+	 * Gets the word zone used to render the text in the textZone
+	 * @return
+	 */
+	public WordZone getWordZone(){
+		return currentWordZone;
+	}
+	/**
+	 * Sets if the text in the TextZone is selectable/highlightable 
+	 * @param flag
+	 */
+	public void setSelectable(boolean flag){
+		selectable = flag;
+	}
+	
+	/**
+	 * Gets the flag that states if the text in the TextZone is selectable/highlightable 
+	 * @return
+	 */
+	public boolean getSelectable(){
+		return selectable;
+	}
+	
+	/**
+	 * Set the size of the font
+	 * @param fs
+	 */
+	public void setFontSize(float fs){
+		fontSize = fs;
+	}
+	
+	/**
+	 * Get the font size
+	 * @return
+	 */
+	public float getFontSize(){
+		return fontSize;
+	}
+	
+	/**
+	 * Set Set the TextZone to receive input from the PApplet (keyboard)
+	 * @param keysFlag
+	 */
+	public void setKeysFromApplet(boolean keysFlag){
+		keysFromApplet = keysFlag;
+	}
+	
+	/**
+	 * Gets the flag that states if the TextZone is to receive input from the PApplet (keyboard)
+	 * @return
+	 */
+	public boolean getKeysFromApplet(){
+		return keysFromApplet;
+	}
+	
+	/**
+	 * Sets the blur flag. If the text should be blurred.
+	 * @param blurFlag
+	 */
+	public void setBlur(boolean blurFlag){
+		blur = blurFlag;
+	}
+	
+	/**
+	 * Gets the blur flag. If the text is set to blurred.
+	 * @return
+	 */
+	public boolean getBlur(){
+		return blur;
+	}
+	
+	/**
+	 * Set the small font used for blurring
+	 * Blurring is achieved by stretching small font
+	 * @param font
+	 */
+	public void setSmallFont(PFont font){
+		sFont = font;
+	}
+	
+	/**
+	 * Set the small font used for blurring
+	 * Blurring is achieved by stretching small font
+	 */
+	public PFont getSmallFont(){
+		return sFont;
+	}
+	
+	/**
+	 * Set the font
+	 * @param font
+	 */
+	public void setFont(PFont font){
+		this.font = font;
+	}
+	
+	/**
+	 * Get the font
+	 */
+	public PFont getFont(){
+		return font;
+	}
+	
+	
 
 	boolean warnDraw() {
 		return false;
@@ -162,11 +425,7 @@ public class TextZone extends Zone {
 		}
 		sFont = applet.createFont("Lucida Sans", 3.0f);
 
-		if (inputText != null) {
-			for (char c : inputText.toCharArray()) {
-				this.addChar(c);
-			}
-		}
+		addText(inputText);
 	}
 
 	public TextZone(int x, int y, int width, int height) {
@@ -187,10 +446,51 @@ public class TextZone extends Zone {
 
 	@Override
 	public void drawImpl() {
-		fill(255);
-		rect(0, 0, width, height);
+		
 	}
 
+	/**
+	 * Gets the text being rendered to the TextZone
+	 * @return
+	 */
+	public String getText(){
+		return currentWordZone.word;
+	}
+	
+	/**
+	 * Deletes all the text being rendered to the TextZone
+	 */
+	public void deleteAllText(){
+		currentWordZone.word = "";
+	}
+	
+	/**
+	 * Deletes a number of characters from the end of the text
+	 * that is being rendered to the TextZone
+	 * @param numChars
+	 */
+	public void delete(int numChars){
+		for(int i = 0; i < numChars; i++){
+			currentWordZone.word = currentWordZone.word.substring(currentWordZone.word.length(), currentWordZone.word.length()-numChars);
+		}
+	}
+	
+	/**
+	 * Add a String of text to the TextZone
+	 * @param text
+	 */
+	public void addText(String text){
+		if (text != null) {
+			for (char c : text.toCharArray()) {
+				this.addChar(c);
+			}
+		}
+	}
+	
+	/**
+	 * Add a char to the TextZone
+	 * @param c
+	 */
 	public void addChar(char c) {
 		if (c == ' ') {
 			this.currentWordZone = new WordZone(currentWordZone.x + currentWordZone.width,
